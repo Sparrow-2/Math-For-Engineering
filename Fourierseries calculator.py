9a13c64a5131fc0bc4777a748b72fc7de8b2817f
Tomasz Wróbel <tomasz.wrobel2@gmail.com>
2022-03-11 18:59:00 +0100

    I guess that's a final version

--- Fourierseries calculator.py
+++ Fourierseries calculator.py
@@ -1,4 +1,4 @@
-import math
+
 
 from sympy import fourier_series, pi, plot
 from sympy.abc import x
@@ -7,17 +7,17 @@ def Calc():
     function_Fourier = input("write down the function")
     l = float(input("give l"))
     s = fourier_series(function_Fourier, (x, -l, l))
-    s10 = s.truncate(n=10)
 
 
-    s100 = s.truncate(n=250)
-    p = plot(function_Fourier,s10,s100, (x, -l, l), show=False, legend=True)
+
+    s100 = s.truncate(n=100)
+    print(s.sigma_approximation(100))
+    p = plot(function_Fourier,s100, (x, -l, l), show=False, legend=True)
     p[0].line_color = 'b'
     p[0].label = 'function'
-    p[1].line_color = 'r'
-    p[1].label = 'of 10'
-    p[2].line_color = 'green'
-    p[2].label = 'of 100'
+
+    p[1].line_color = 'red'
+    p[1].label = 'of 100'
 
 
     p.show()
